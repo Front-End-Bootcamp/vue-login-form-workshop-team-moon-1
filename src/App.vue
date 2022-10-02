874dad34af4b4ffe63da9128b51d27f1da892114
trombonist76 <mahmut.hg@gmail.com>
2022-10-02 11:27:05 +0300

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import Register from './components/Register.vue';
+import Login from './components/Login.vue';
+import { ref } from 'vue';
+	const isLogin = ref(false)
 </script>
 
 <template>
-	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-			asdasd
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
+	<Register/>
+	<Login v-if="isLogin"/>
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
 
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,7 +1,24 @@
 <script setup>
-	
+import NameInput from './Inputs/NameInput.vue';
+import MailInput from './Inputs/MailInput.vue';
+import PhoneInput from './Inputs/PhoneInput.vue';
+import PasswordInput from './Inputs/PasswordInput.vue';
+
 </script>
 
 <template>
-
+	<div>
+		<h1>Register</h1>
+		<div>
+			<NameInput />
+			<MailInput />
+			<PhoneInput />
+			<PasswordInput />
+			<PasswordInput />
+		</div>
+		<div>
+			<button>Register</button>
+			<button>Have an account</button>
+		</div>
+	</div>
 </template>
